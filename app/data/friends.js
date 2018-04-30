04234736f30c0422e9b36a76399439a4f5871225
jfleck91 <jfleck91@gmail.com>
2018-04-30 09:58:50 -0700

    4th update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,29 @@
-var surveyArray = [
+var friendsData = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      }
+      name: "Bruce Banner",
+      photo:"http://static.tvtropes.org/pmwiki/pub/images/mcu_bruce_banner.png",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Elwood Blues",
+      photo:"https://orig00.deviantart.net/cbb8/f/2015/079/9/6/profile_picture_by_elwood__blues-d8mgu86.jpg",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+
+    {
+      name: "Dr. Evil",
+      photo:"https://at-cdn-s01.audiotool.com/2012/05/28/documents/ei368KjdlP5qy6gILDLtH9cEAf6H/0/cover256x256-b31436c2a9bc4645b1ad67bc09705cd7.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+ 
+    {
+      name: "Princess Leia",
+      photo:"http://images.techtimes.com/data/images/full/168082/princess-leia-organa.jpg?w=600",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
   ];
   
   
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = waitingArray;
   
+  // Export to API Route
+  module.exports = friendsData;
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,17 +1,25 @@
-// DEPENDENCIES
-var path = require("path");
+console.log('HTML Route Connected Successfully');
 
 
-// ROUTING
-module.exports = function(app) {
+// Node Dependencies
+var path = require('path');
 
 
-// HTML GET Requests
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+// Includes Two Routes
+function htmlRoutes(app) {
+
+  // A GET Route to /survey which should display the survey page.
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
-app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+  // A default USE route that leads to home.html which displays the home page.
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
-};
+
+}
+
+
+// Export for use in main server.js file
+module.exports = htmlRoutes;
