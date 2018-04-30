a9f5eaf2dc876ebbb21614dba9125ba97ddbf52d
jfleck91 <jfleck91@gmail.com>
2018-04-30 09:13:51 -0700

    update3

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,20 +1,15 @@
 // LOAD DATA
 var freindsData = require("../data/friends");
 
-
 // ROUTING
 module.exports = function(app) {
-
  
 // API GET Requests
-
 app.get("/api/friends", function(req, res) {
     res.json(tableData);
   });
 
-
 // API POST Requests
-
 app.post("/api/friends", function(req, res)
 
-// if/else
+//if else goes here
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -7,14 +7,11 @@ module.exports = function(app) {
 
 
 // HTML GET Requests
-
-
 app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  // If no matching route is found default to home
-  app.get("/home", function(req, res) {
+app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
