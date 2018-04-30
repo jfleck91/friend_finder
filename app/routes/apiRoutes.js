69140d5566c3e4127e34e0b04cfa56b34bbafebb
jfleck91 <jfleck91@gmail.com>
2018-04-29 17:32:14 -0700

    1st update

---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,20 @@
+// LOAD DATA
+var freindsData = require("../data/friends");
+
+
+// ROUTING
+module.exports = function(app) {
+
+ 
+// API GET Requests
+
+app.get("/api/friends", function(req, res) {
+    res.json(tableData);
+  });
+
+
+// API POST Requests
+
+app.post("/api/friends", function(req, res)
+
+// if/else
